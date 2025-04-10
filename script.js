d651aa0b3f2bbcd75921d97685d4aad240281cdf
Deepak Rizal <deepak2000rizal@gmail.com>
2025-04-10 19:26:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const inputBox = document.querySelector(".input-box");
+const checkStrength = document.querySelector(".check-strength");
+const passwordBox = document.querySelector(".password-box");
+const passwordSuccessMessage = document.querySelector(
+  ".password-success-message"
+);
+
+function resetError() {
+  const errorDiv = document.querySelector(".error");
+
+  if (errorDiv) {
+    errorDiv.textContent = "";
+  }
+}
+
+function checkIsPasswordIsValid(password) {
+  const checkLength = password.length >= 8;
+  const checkSpecialCharacter = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  const hasCapitalLetter = /[A-Z]/.test(password);
+  const hasNumber = /\d/.test(password);
+
+  return checkLength && checkSpecialCharacter && hasCapitalLetter && hasNumber;
+}
+
+function generateError() {
+  const paragraph = document.createElement("div");
+  paragraph.classList.add("error");
+  paragraph.textContent =
+    "password should be minimum 8 of characters, there should be a capital letter, there should be a special character, there should be a number,";
+  passwordBox.appendChild(paragraph);
+}
+
+inputBox.addEventListener("focusout", (e) => {
+  resetError();
+  if (!checkIsPasswordIsValid(e.target.value)) {
+    generateError();
+  }
+});
+
+checkStrength.addEventListener("click", () => {
+  resetError();
+  if (checkIsPasswordIsValid(inputBox.value)) {
+    passwordSuccessMessage.textContent =
+      "Congratulations! you can go ahead with this password!";
+  } else {
+    generateError();
+  }
+});
